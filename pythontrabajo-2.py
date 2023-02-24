201b0450ab36f13d5d59ad3278c825e450687cd3
Atter12xd <1485275@senati.pe>
2023-02-24 13:16:42 -0500

    Procesamiento

--- pythontrabajo-2.py
+++ pythontrabajo-2.py
@@ -3,6 +3,11 @@
 
 # creando nuestro Lista de numeros 
 numeros = [15, 24, 35, 42, 50, 66, 73]
-
 # Lista vacia para almacenar los numeros divisibles entre 5
 Divisible = []
+# Recorremos cada elemento en la lista de numeros
+for x in numeros:
+    #Aca va a verificar si el numero es divisible entre 5
+    if x % 5 == 0:
+      # si es divisible, se agregara a la lista de numeros divisible entre 5 
+      Divisible.append(x) # Append se usa para agrega un ítem al final de la lista
