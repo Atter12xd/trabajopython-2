595a0e8f50f2ab601d7ee0aaf1945ccdd582c393
Atter12xd <1485275@senati.pe>
2023-02-24 11:15:06 -0500

    Trabajo todo en un archivo

--- pythontrabajo-2.py
+++ pythontrabajo-2.py
@@ -1,11 +1,9 @@
 # Vamos a desarrollar un algoritmo en la que muestra la suma del numero actual y su anterior
 # Vamos a indicar cuantos numeros desea
 a = int(input("Ingrese el valor: "))
-
 # Ahora usaremos las funciones "for" "in" "range"
-for i in range (1, a ):
+for i in range (1, a +1):
     a = i
     suma = a + (a-1)
 # Ahora tocaria sumar del numerol actual y su anterior 
-
     print("la suma de", a, "+", a - 1, "es", suma)
