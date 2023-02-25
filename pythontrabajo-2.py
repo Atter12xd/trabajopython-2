192bdb9765c92d7779e15750388849e9149594b2
unknown <Atter12xd>
2023-02-25 09:28:07 -0500

    Ejecutando el progama y poniendo la funcion end=

--- pythontrabajo-2.py
+++ pythontrabajo-2.py
@@ -1,4 +1,6 @@
 # vamos a usar las funciones for y range para dar un rango ya que el trabajo es hasta el 5 sera un rango de (6)
 for x in range(1, 6):  
     for j in range(x):
-        
+         # Ya poniendo la base, estaria listo para ejecutarse, entonces vamos a imprimir la variable "x" y tambien usaremos "end=" ")"
+        print(x,  end=' ') #"end=' '" significa es lo que desea imprimir después de que se haya ejecutado la instrucción print.
+    print()
